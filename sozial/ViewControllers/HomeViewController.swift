eb2466981b0066b5e0b415c671e539ea84e6c562
Shivang Ranjan <shivang12134@gmail.com>
2019-09-23 10:11:16 +0530

    [feat]: News Feed

--- sozial/ViewControllers/HomeViewController.swift
+++ sozial/ViewControllers/HomeViewController.swift
@@ -1,18 +1,14 @@
-//
-//  HomeViewController.swift
-//  sozial
-//
-//  Created by Shivang Ranjan on 31/08/19.
-//  Copyright © 2019 shivangcodes. All rights reserved.
-//
+
 
 import UIKit
 import FirebaseAuth
 
 class HomeViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
         
     }
 
@@ -35,3 +31,20 @@ class HomeViewController: UIViewController {
     }
     
 }
+
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell", for: indexPath)
+        cell.textLabel?.text = "\(indexPath.row)"
+        cell.backgroundColor = UIColor.red
+        
+        return cell
+    }
+    
+    
+}
+

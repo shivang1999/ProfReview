03fd24c42b0de7f264340236d630f9d489b228a8
Shivang Ranjan <shivang12134@gmail.com>
2019-09-23 10:40:20 +0530

    [feat]: Grab post data from firebase database

--- sozial/ViewControllers/HomeViewController.swift
+++ sozial/ViewControllers/HomeViewController.swift
@@ -2,6 +2,7 @@
 
 import UIKit
 import FirebaseAuth
+import FirebaseDatabase
 
 class HomeViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
@@ -9,7 +10,14 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
+        loadPosts()
         
+    }
+    
+    func loadPosts() {
+        Database.database().reference().child("posts").observe(.childAdded) { (snapshot: DataSnapshot) in
+            print(snapshot.value)
+        }
     }
 
     @IBAction func logout_TouchUpInside(_ sender: Any) {
@@ -34,7 +42,7 @@ class HomeViewController: UIViewController {
 
 extension HomeViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return 100
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
